4973a3cff3a47f0502b88b29e53d970a71e9e269
MyJineng <98989716+MyJineng@users.noreply.github.com>
2024-04-04 19:20:41 -0400

    added scatter plot and bar plots

--- mktsmall.py
+++ mktsmall.py
@@ -18,15 +18,54 @@ mktsmall["instagram %"] = round((mktsmall['InstagramUsers2023'] / mktsmall["user
 
 idf = pd.read_csv(rf'{path}\top_1000_instagramers.csv')
 idf = idf["country"].value_counts().reset_index()
-idf = idf.rename(columns={'country': 'influencers','index': 'country', })
-mktsmall = mktsmall.merge(idf, how='inner', on="country")
-mktsmall = mktsmall[['country', 'instagram %', 'influencers']]
-bargraph
-multi_plot = mktsmall.plot(kind="bar", figsize=(20,5))
-multi_plot.set_xticklabels(mktsmall["country"], rotation=45)
+idf = idf.rename(columns={'country': 'insta_influencers','index': 'country', })
+
+plot = idf.plot(kind="bar", figsize=(20,5))
+plot.set_xticklabels(idf["country"], rotation=45)
 plt.title("Top 100 Instagram Influencers by Country")
 plt.xlabel("Country")
-plt.show()
 plt.tight_layout()
+plt.show()
 
+tdf = pd.read_csv(rf'{path}\top_1000_tiktok.csv')
+tdf = tdf["country"].value_counts().reset_index()
+tdf = tdf.rename(columns={'country': 'tiktok_influencers','index': 'country', })
 
+plot = tdf.plot(kind="bar", figsize=(20,5))
+plot.set_xticklabels(tdf["country"], rotation=45)
+plt.title("Top 100 Tiktok Influencers by Country")
+plt.xlabel("Country")
+plt.tight_layout()
+plt.show()
+
+ydf = pd.read_csv(rf'{path}\top_1000_yt.csv')
+ydf = ydf["country"].value_counts().reset_index()
+ydf = ydf.rename(columns={'country': 'yt_influencers','index': 'country', })
+
+plot = ydf.plot(kind="bar", figsize=(20,5))
+plot.set_xticklabels(ydf["country"], rotation=45)
+plt.title("Top 100 Youtube Influencers by Country")
+plt.xlabel("Country")
+plt.tight_layout()
+plt.show()
+
+print(idf, tdf, ydf)
+
+mktsmall = mktsmall.merge(idf, how='outer', on="country")
+mktsmall = mktsmall[['country', 'instagram %', "tiktok %", "youtube %", 'insta_influencers']]
+mktsmall = mktsmall.merge(tdf, how='outer', on="country")
+mktsmall = mktsmall[['country', 'instagram %', "tiktok %", "youtube %", 'insta_influencers', 'tiktok_influencers']]
+mktsmall = mktsmall.merge(ydf, how='outer', on="country")
+mktsmall = mktsmall[['country', 'instagram %', "tiktok %", "youtube %", 'insta_influencers', 'tiktok_influencers', 'yt_influencers',]]
+#bargraph
+# multi_plot = mktsmall.plot(kind="bar", figsize=(20,5))
+# multi_plot.set_xticklabels(mktsmall["country"], rotation=45)
+# plt.title("Top 100 Influencers by Country")
+# plt.xlabel("Country")
+# plt.tight_layout()
+# plt.show()
+#scatterplot
+# mktsmall.plot.scatter(x = 'instagram %', y = 'insta_influencers', s = 100, edgecolor ="red");
+# mktsmall.plot.scatter(x = 'tiktok %', y = 'tiktok_influencers', s = 100, edgecolor ="blue");
+# mktsmall.plot.scatter(x = 'youtube %', y = 'yt_influencers', s = 100, edgecolor ="green");
+# plt.show()
